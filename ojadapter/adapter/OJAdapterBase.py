35b8e746d708fb0d27b6627c877796f865138ced
fish-ball <57082212@qq.com>
2019-08-01 15:22:11 +0800

    修正了 image title 里面含有方括号时的抓取问题

--- ojadapter/adapter/OJAdapterBase.py
+++ ojadapter/adapter/OJAdapterBase.py
@@ -96,15 +96,15 @@ class OJAdapterBase(object):
         :return: 返回 markdown 纯文本字符串
         """
         import re
-        # print(content)
+        print(content)
 
         def replace_img_src(match):
             img_url, = match.groups()
             match = match.group()
-            # print(img_url, match)
+            print(img_url, match)
             return match.replace(img_url, self.download_file(img_url, 'images', current_url))
 
-        result = re.sub(r'!\[(?:\\\]|[^]])*\]\((\\\)|[^)]+)\)', replace_img_src, content)
+        result = re.sub(r'!\[(?:\\\]|[^]])*\]\(((?:\\\)|[^)])+)\)', replace_img_src, content)
         return result
 
     def sanitize_html(self, content, current_url=''):
